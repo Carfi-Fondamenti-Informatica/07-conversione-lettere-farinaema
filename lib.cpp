a9e88b7a6067641c50467be82cdb6c49c472efdf
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-05 16:04:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+bool carattere( char a ){
+    int n;
+    n = (int) a;
+
+    if ( n>= 48 and n<=57){
+        return false;
+    }
+
+    return true;
+}
+
+void conversione (char &a){
+
+    int n;
+    n = (int) a;
+    if( n>=65 and n<=90){
+
+        n = n + 32;
+        a = (char) n;
+    }
+
+    n= n-32;
+    a = (char) n;
+}
+
+int main(){
+    return 0;
+}
