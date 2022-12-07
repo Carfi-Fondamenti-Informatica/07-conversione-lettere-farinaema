489810bbf5b1c7c408dbb8119f7cb15ca30838d6
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-07 09:34:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,12 +5,14 @@ using namespace std;
 bool carattere( char a ){
     int n;
     n = (int) a;
-
-    if ( n>= 48 and n<=57){
-        return false;
+    cout << n << endl;
+    if ( n>= 65 and n<=90 ){
+        return true;
+    } else if ( n>=97 and n<=122) {
+        return true;
     }
 
-    return true;
+    return false;
 }
 
 char conversione (char &a){
