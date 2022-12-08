5337e7e90c0a00576f88fe01eb3fd0a999b3dd65
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-08 20:44:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,26 +3,19 @@
 using namespace std;
 
 bool conversione( char &a ){
-    int n;
-    n = (int) a;
-    cout << n << endl;
-    if ( (n>= 65 and n<=90) or (n >= 97 and n <= 122) ){
        
         if( n>=65 and n<=90){
 
         n = n + 32;
         a = (char) n;
-    
+            return true;
         } else if ( n >= 97 and n <= 122){
 
         n = n - 32;
         a = (char) n;
-   
-        }
-        return true;
-    } else {
+           return true;
+        } else {
 
-    return false;
-        
-    }
+        return false;
+        }
 }
