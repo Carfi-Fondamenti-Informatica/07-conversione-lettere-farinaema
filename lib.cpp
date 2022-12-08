193e6d13587f3ccfa86d968e699d7c3dd347dbbe
farinaema <117036663+farinaema@users.noreply.github.com>
2022-12-08 20:48:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -7,7 +7,7 @@ bool conversione( char &a ){
         if( a>=65 and a<=90){
 
         a = a + 32;
-        return true
+        return true;
                
         } else if ( a >= 97 and a <= 122){
 
